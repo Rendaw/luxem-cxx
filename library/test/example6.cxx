e979cefe510bb9fa1577495ee3e3db4a55566d11
Rendaw <rendaw@zarbosoft.com>
2015-02-08 18:44:58 +0900

    Modified example6 to handle input, display output

--- library/test/example6.cxx
+++ library/test/example6.cxx
@@ -1,5 +1,7 @@
 #include "../luxem.h"
 
+#include <iostream>
+
 int main(void)
 {
 	std::vector<int> numbers;
@@ -18,6 +20,11 @@ int main(void)
 				}
 			);
 	});
+	reader.feed("4, 12, {\"key\": \"I AM A VALUE TOO\"}, 3");
+	std::cout << "numbers:";
+	for (auto number : numbers) std::cout << " " << number;
+	std::cout << std::endl;
+	std::cout << "text: " << text << std::endl;
 
 	return 0;
 }
